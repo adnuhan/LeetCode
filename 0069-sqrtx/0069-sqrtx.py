77f755107a5a03d9032e9a2c7fcbdf3a0c7763a0
Nahunda Bekele <138674654+NahundaBekele@users.noreply.github.com>
2024-05-03 20:35:53 +0300

    Time: 1398 ms (9.80%), Space: 16.6 MB (41.52%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        output = 0
+        odd = 1
+        rem = x
+        while rem >= odd:
+            rem = rem - odd
+            odd += 2
+            output += 1
+        return output
