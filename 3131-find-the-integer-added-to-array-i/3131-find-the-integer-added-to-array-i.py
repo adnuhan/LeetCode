740f60516ba0dfb1b9b3fb59c6d440123a77e05d
Nahunda Bekele <138674654+NahundaBekele@users.noreply.github.com>
2024-05-21 11:30:48 +0300

    Time: 19 ms (98.78%), Space: 11.7 MB (41.61%) - LeetHub

---
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addedInteger(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        return max(nums2) - max(nums1) 
+        
