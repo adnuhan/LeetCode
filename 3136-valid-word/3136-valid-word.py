919e9dd2531c71651dd7f1a35d656f2fc1cae1ab
Nahunda Bekele <138674654+NahundaBekele@users.noreply.github.com>
2024-05-22 17:52:45 +0300

    Time: 15 ms (66.19%), Space: 11.8 MB (25.57%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isValid(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        vowel = "aeiouAEIOU"
+        character = "@#$"
+        consonant = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+        
+        vowels = 0
+        consonants = 0
+        characters = 0
+        
+        for i in range(len(word)):
+            if word[i] in character:
+                characters += 1
+            elif word[i] in vowel:
+                vowels += 1
+            elif word[i] in consonant:
+                consonants += 1
+        
+        if len(word) >= 3 and vowels > 0 and consonants > 0 and characters == 0:
+            return True
+        else:
+            return False
