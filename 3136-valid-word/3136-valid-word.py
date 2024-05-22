d7838b96940e42238056559c8e8694fb101f9209
Nahunda Bekele <138674654+NahundaBekele@users.noreply.github.com>
2024-05-22 17:53:42 +0300

    Time: 13 ms (74.86%), Space: 11.7 MB (25.57%) - LeetHub

--- 3136-valid-word/3136-valid-word.py
+++ 3136-valid-word/3136-valid-word.py
@@ -10,17 +10,16 @@ class Solution(object):
         
         vowels = 0
         consonants = 0
-        characters = 0
         
         for i in range(len(word)):
             if word[i] in character:
-                characters += 1
+                return False
             elif word[i] in vowel:
                 vowels += 1
             elif word[i] in consonant:
                 consonants += 1
         
-        if len(word) >= 3 and vowels > 0 and consonants > 0 and characters == 0:
+        if len(word) >= 3 and vowels > 0 and consonants > 0:
             return True
         else:
             return False
