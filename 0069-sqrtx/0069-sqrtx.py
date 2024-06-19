18f01a92e111e8c95f1b0ee20c466394d6ef0f06
Nahunda Bekele <138674654+NahundaBekele@users.noreply.github.com>
2024-06-19 22:40:02 +0300

    Time: 1398 ms (9.80%), Space: 16.6 MB (41.52%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -3,8 +3,10 @@ class Solution:
         output = 0
         odd = 1
         rem = x
+        
         while rem >= odd:
             rem = rem - odd
             odd += 2
             output += 1
+    
         return output
