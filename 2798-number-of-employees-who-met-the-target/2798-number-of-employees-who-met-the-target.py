f938fe3ccec616b0ea17648805a33af700389090
Nahunda Bekele <138674654+adnuhan@users.noreply.github.com>
2024-07-16 20:03:22 +0300

    Time: 41 ms (76.77%), Space: 16.5 MB (58.96%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
@@ -1,10 +1,21 @@
 class Solution:
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
         
+        i = 0
+        j = len(hours) - 1
         output = 0
         
-        for num in hours:
-            if num >= target:
-                output += 1
-
+        while i <= j:
+            if i == j:
+                if hours[i] >= target:
+                    output += 1
+            else:
+                if hours[i] >= target:
+                    output += 1
+                if hours[j] >= target:
+                    output += 1
+            i += 1
+            j -= 1
+        
         return output
+        
