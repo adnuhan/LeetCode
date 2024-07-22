6667e64dc39596bbff748e3279fc17a6bc464383
Nahunda Bekele <138674654+adnuhan@users.noreply.github.com>
2024-07-22 14:32:42 +0300

    Time: 30 ms (91.82%), Space: 16.7 MB (19.19%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -9,11 +9,9 @@ class Solution:
                 hash_map[num] = 1
             else:
                 hash_map[num] += 1
-        
-        
+
         for value in hash_map.values():
             if value > 0:
                 output += int(value * (value - 1) / 2)
-        
-        
+
         return output
