839870f7e91cb41a10af54dae4ace66bc9954e47
Nahunda Bekele <138674654+adnuhan@users.noreply.github.com>
2024-07-23 12:37:57 +0300

    Time: 231 ms (59.65%), Space: 22.7 MB (79.32%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
@@ -3,17 +3,16 @@ class Solution:
         
         index = 0
         output = 0        
-        
+
         if ruleKey == "type":
             index = 0
         elif ruleKey == "color":
             index = 1
         else:
             index = 2
-            
+
         for item in items:
             if item[index] == ruleValue:
                 output += 1
-            
+
         return output
-            
