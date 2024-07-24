6d2eb7dd990ecb2dcf381783fd436ac3034d9797
Nahunda Bekele <138674654+adnuhan@users.noreply.github.com>
2024-07-24 19:02:38 +0300

    Time: 1116 ms (52.72%), Space: 25.1 MB (36.96%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
@@ -1,6 +1,6 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        
+
         mapped = []
         output = []
         shuffled = ""
@@ -11,8 +11,8 @@ class Solution:
                 shuffled += str(mapping[int(num[y])])
             mapped.append(shuffled)
             shuffled = ""
-        
+
         for z in sorted(range(len(mapped)), key=lambda k: int(mapped[k])):
             output.append(nums[z])
-        
+
         return output
