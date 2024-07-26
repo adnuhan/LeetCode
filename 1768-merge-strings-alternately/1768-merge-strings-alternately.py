ae824ac3cc703f566a07fbf25ac87459d45b1f48
Nahunda Bekele <138674654+adnuhan@users.noreply.github.com>
2024-07-26 12:16:30 +0300

    Time: 30 ms (87.66%), Space: 16.6 MB (25.69%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,6 +1,6 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        
+
         output = []
         length = max(len(word1), len(word2))
 
@@ -9,5 +9,6 @@ class Solution:
                 output.append(word1[i])
             if i < len(word2):
                 output.append(word2[i])
-                
+    
         return "".join(output)
+    
