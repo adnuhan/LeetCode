3d634c49e2a7e8ed8406baf6ec4626d0f1cd39b9
Nahunda Bekele <138674654+adnuhan@users.noreply.github.com>
2024-08-05 15:40:28 +0300

    Time: 27 ms (93.34%), Space: 16.4 MB (74.47%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        
+        hashmap = {}
+        s = s.split()
+        
+        if len(pattern) != len(s):
+            return False
+        
+        for i in range(len(pattern)):
+            print(hashmap)
+            if pattern[i] not in hashmap and s[i] not in hashmap.values():
+                hashmap[pattern[i]] = s[i]
+            elif pattern[i] not in hashmap or hashmap[pattern[i]] != s[i]:
+                return False
+        
+        return True
