312413c9df56005756d3927733e0a61332888b84
Nahunda Bekele <138674654+adnuhan@users.noreply.github.com>
2024-08-10 22:25:51 +0300

    Time: 41 ms (30.55%), Space: 16.6 MB (20.72%) - LeetHub

---
+++ 3468-find-the-encrypted-string/3468-find-the-encrypted-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getEncryptedString(self, s: str, k: int) -> str:
+        
+        output = []
+        index = 0
+        
+        if k > len(s):
+            k = k % len(s)
+
+        for n in range(len(s)):
+            index = n + k
+            if index < len(s):
+                output.append(s[index])
+            else:
+                index -= len(s)
+                output.append(s[index])
+
+        return "".join(output)
+    
